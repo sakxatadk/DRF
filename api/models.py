3cb567ccc17a40a8e5bc57f8142d2f70a1fd1724
sakxatadk <sakshyatadk04@gmail.com>
2026-05-02 18:01:03 +0530

    model changes

--- api/models.py
+++ api/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
